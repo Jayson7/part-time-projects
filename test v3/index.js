b01101420c2173cacdc165344b1a3e9f6e94734d
jayson7 <lexxiijoo70@gmail.com>
2021-04-02 23:43:27 +0100

    updated

---
+++ test v3/index.js
@@ -0,0 +1,41 @@
+window.addEventListener("load", () => {
+
+
+    var nav = document.querySelector("nav");
+    var times = document.querySelector(".fa-times");
+    var bars = document.querySelector(".fa-bars");
+    bars.addEventListener("click", () => {
+        times.style.display = "flex";
+        bars.style.display = "none";
+        nav.style.display = "grid";
+        window.addEventListener("resize", () => {
+            times.style.display = "none";
+
+
+        })
+
+    })
+    times.addEventListener("click", () => {
+        times.style.display = "none";
+        bars.style.display = "flex";
+        nav.style.display = "none";
+
+
+    })
+    window.addEventListener("resize", () => {
+        if (window.innerWidth > 900) {
+            bars.style.display = "none";
+            nav.style.display = "flex"
+            times.style.display = "none"
+
+        }
+        if (window.innerWidth < 900) {
+            bars.style.display = "flex";
+            nav.style.display = "none"
+
+        }
+
+
+    })
+
+})
